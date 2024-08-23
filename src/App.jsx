3658a12cdaa2a2728fe8f4005ca47d5a79c6892d
rehanismael <154280884+rehanismael@users.noreply.github.com>
2024-08-23 15:51:04 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from './Components/About/About'
+import Applications from './Components/Applications/Applications'
+import Hero from './Components/Hero/Hero'
+
+function App() {
+  return <>
+  <div>
+    <Hero />
+    <About />
+    <Applications />
+    </div>
+  </>
+}
+
+export default App
