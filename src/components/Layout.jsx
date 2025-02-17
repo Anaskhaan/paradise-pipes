06fb7d036d8ced398e8109bed8b1623d760a06fb
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-02-17 14:06:48 +0500

    layout updated

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
