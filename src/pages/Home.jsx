082a52d8cb3aeeec4878efd30ffe52e8f92fbfb5
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-02-19 14:23:10 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Basecontact from "../components/Home-Components/Basecontact";
-import Contact from "../components/Home-Components/Contact";
-import FAQs from "../components/Home-Components/FAQs";
-import Features from "../components/Home-Components/Features";
-import Hero from "../components/Home-Components/Hero";
-import ProductRange from "../components/Home-Components/ProductRange";
-import WhyChooseUs from "../components/Home-Components/WhyChooseus";
+import Basecontact from "../components/HomeComponents/Basecontact";
+import Contact from "../components/HomeComponents/Contact";
+import FAQs from "../components/HomeComponents/FAQs";
+import Features from "../components/HomeComponents/Features";
+import Hero from "../components/HomeComponents/Hero";
+import ProductRange from "../components/HomeComponents/ProductRange";
+import WhyChooseUs from "../components/HomeComponents/WhyChooseus";
 
 export default function Home() {
   return (
